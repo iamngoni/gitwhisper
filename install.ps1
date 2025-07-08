4fd11941c956f0606cb41f08afadb028cedd38ff
Ngonidzashe Mangudya <imngonii@gmail.com>
2025-07-08 09:56:23 +0200

    fix: 🐛 Correct admin elevation prompt in install script

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 # Relaunch as admin if not elevated
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-  Write-Host "🛡️  Elevating to administrator..."
-  Start-Process powershell.exe "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+  $args = "-NoExit", "-ExecutionPolicy", "Bypass", "-File", "`"$PSCommandPath`""
+  Start-Process powershell.exe -Verb RunAs -ArgumentList $args
   exit
 }
 
