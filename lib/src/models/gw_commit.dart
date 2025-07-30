d7d986f777b1f69940fbc955cff9bd62e473f88c
Ngonidzashe Mangudya <imngonii@gmail.com>
2025-07-30 11:53:21 +0200

    feat: ✨ Add logo image and commit model, replace emoji with logo in HTML files

---
+++ lib/src/models/gw_commit.dart
@@ -0,0 +1,14 @@
+//
+//  gitwhisper
+//  gw_commit.dart
+//
+//  Created by Ngonidzashe Mangudya on 2025/07/18.
+//  Copyright (c) 2025 Codecraft Solutions. All rights reserved.
+//
+
+class GwCommit {
+  const GwCommit(this.commit, this.model, this.dateTime);
+  final String commit;
+  final String model;
+  final DateTime dateTime;
+}
